692ab9362cd5a1af878c4f5dc5a782924276011c
Haidar <Tiger191110@gmail.com>
2026-09-07 15:58:52 +0700

    feat: menambahkan endpoint router untuk list produk

---
+++ backend/src/routes/productRoutes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+import { products } from '../data/products';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json(products);
+});
+
+export default router;
